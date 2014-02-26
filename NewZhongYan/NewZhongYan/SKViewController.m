a210ab21f6a4910d77add5ad22ce2ec319628078
lilin87788 <lilin87788@163.com>
2014-02-26 11:22:39 +0800

    提交主界面的修改
    主界面的修改和数据元信息的修改

--- NewZhongYan/NewZhongYan/SKViewController.m
+++ NewZhongYan/NewZhongYan/SKViewController.m
@@ -295,7 +295,7 @@
 -(void)initPageController
 {
     pageController = [[SMPageControl alloc] initWithFrame:CGRectMake((320 - 150)/2., BottomY - 49 - 30, 150, 40)];
-    [pageController setHidden:YES];
+    //[pageController setHidden:YES];
     [pageController setIndicatorDiameter:8];
     [pageController setNumberOfPages:2];
     [pageController setHidesForSinglePage:YES];
@@ -376,6 +376,7 @@
     [self copyXMLToDocument];
     [self initNavBar];
     [self initPageController];
+    [self initSetting];
     if (isFirstLogin) {
         SKLoginViewController* loginController = [[APPUtils AppStoryBoard] instantiateViewControllerWithIdentifier:@"loginController"];
         [FileUtils setvalueToPlistWithKey:@"EPSIZE" Value:@"5"];
